584a01a9f8e8c33fdb58b54e4a5f186b0c1fa54c
Maximilien Bey <bey.maximilien@gmail.com>
2024-03-07 17:56:37 +0100

    Remove ConfigModule from MovieModule

--- src/cinema.module.ts
+++ src/cinema.module.ts
@@ -1,22 +1,11 @@
 import { CommonModule } from "@app/common"
 import { Module } from "@nestjs/common"
-import { ConfigModule } from "@nestjs/config"
-import * as joi from "joi"
 
 import { CinemaController } from "./cinema.controller"
 import { CinemaService } from "./cinema.service"
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validationSchema: joi.object({
-        DATABASE_URL: joi.string().required(),
-      }),
-      envFilePath: "./apps/cinema/.env",
-    }),
-    CommonModule,
-  ],
+  imports: [CommonModule],
   controllers: [CinemaController],
   providers: [CinemaService],
 })
